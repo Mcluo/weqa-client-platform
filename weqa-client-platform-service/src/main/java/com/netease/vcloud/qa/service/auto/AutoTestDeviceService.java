9900c93dcd506d6eff399d6b1b1ce466d3f0b628
叶果 <>
2023-01-31 15:46:08 +0800

    add device_id

--- weqa-client-platform-service/src/main/java/com/netease/vcloud/qa/service/auto/AutoTestDeviceService.java
+++ weqa-client-platform-service/src/main/java/com/netease/vcloud/qa/service/auto/AutoTestDeviceService.java
@@ -93,11 +93,7 @@ public class AutoTestDeviceService {
         clientAutoDeviceInfoDO.setRun((byte)0);
         clientAutoDeviceInfoDO.setAlive((byte)1);
         int count = clientAutoDeviceInfoDAO.insertNewDeviceInfo(clientAutoDeviceInfoDO) ;
-        if (count>0){
-            return true ;
-        }else {
-            return false ;
-        }
+        return count > 0;
     }
 
     public List<DeviceInfoVO> getDeviceInfoList(List<Long> deviceIdList){
